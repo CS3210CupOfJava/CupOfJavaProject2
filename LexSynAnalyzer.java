d87c2a5a591a44abe09380f067d6f418a9d0a88d
Jacob Sliva <jsliva@msudenver.edu>
2023-09-27 17:14:15 -0600

    Added structure for methods

--- LexSynAnalyzer.java
+++ LexSynAnalyzer.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /*  Project 2: Lexical and Syntax Analysis
  *  
  * Write a program language of your choice that takes in a Java program as an input and does the 
@@ -19,8 +20,37 @@
  * program, and the number of time keyword “public” is used.
  */
 class LexSynAnalyzer {
+
+    //Check to make sure all decision structures (if, if-else, if-else-if, switch) use curly braces
+    //appropriately. If not, fix it.
+    public void checkConditionals(){
+
+    }
+
+    //Check to make sure all loop structures (while, do-while, for) use curly braces
+    //appropriately. If not, fix it.
+    public void checkLoops(){
+
+    }
+    //Check to make sure all the method structure is syntactically correct. If not, fix it.
+    public void checkSyntax() {
+
+    }
+
+    //scan through entire input java file, this method will also be our counting method for the word 'public' as
+    //a keyword.
+    public void scanFile(){
+
+    }
+    //print the output content after running this program, just used for printing the amount of times public
+    //was used, not sure what else we want to use this for yet.
+    public void printOutput(){
+
+    }
+
     public static void main(String []args)
     {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Hello, World!");
     }
 }
